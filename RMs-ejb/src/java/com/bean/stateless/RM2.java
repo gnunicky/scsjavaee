d7564d79a5561ef79e504e1c1d1e147c25777758
leandro <leandro@192.168.1.105>
2016-02-01 11:17:13 +0100

    Init

---
+++ RMs-ejb/src/java/com/bean/stateless/RM2.java
@@ -0,0 +1,58 @@
+package com.bean.stateless;
+
+import com.Offer;
+import com.Reservation;
+import com.bean.singleton.StateLocal;
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Leandro
+ */
+@Stateless(mappedName = "RM2Bean")
+public class RM2 extends RM implements RMLocal{
+
+    @EJB(beanName = "State2")
+    private StateLocal state;
+
+    @Resource(name = "ID")
+    private int ID;
+    
+
+    @Override
+    public void writeDB(Offer o){        
+        System.out.println("RM"+getID()+" I've write DB"+getID());       
+        state.getDB().add(o);
+    }
+    
+    @Override
+    public void writeDB(Reservation r){
+        System.out.println("RM"+getID()+" I've write DB"+getID());
+        
+        String offerID=r.getOfferID();
+        String passengerID=r.getPassengerID();
+        
+        for(Offer o:state.getDB()){
+            if(o.getOfferID().equalsIgnoreCase(offerID))
+                o.addPassenger(passengerID);
+        }
+    }
+
+    @Override
+    public Object readDB() {
+        System.out.println("RM"+getID()+" I've read DB"+getID());
+        return state.getDB();
+    }
+
+    @Override
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public StateLocal getState() {
+        return state;
+    }
+}
